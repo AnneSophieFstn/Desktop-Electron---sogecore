889f231cc21d44f400293a7fdfcb21674a58f9c6
Anne-Sophie <annesophie.faustino@gmail.com>
2023-04-07 13:28:37 +0400

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+const { Notification, BrowserWindow } = require("electron");
+const { getConnection } = require("./database");
+
+let window;
+
+function createWindow() {
+  window = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true, //responsive design
+      contextIsolation: false,
+    },
+  });
+
+  window.loadFile("./src/ui/index.html");
+}
+
+module.exports = {
+  createWindow,
+};
